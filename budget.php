fe897e67629bb72113e5b8be8244f197c2f876d0
Simon Sägesser <simi_saegi@hotmail.com>
2016-11-10 13:53:41 +0100

    budget_01

--- budget.php
+++ budget.php
@@ -4,35 +4,36 @@
     session_destroy();
 
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Willkommen</title>
-</head>
-<body>
-	<div class="container">
-		<div class="row row-centered">
-			<div class="col-md-12">
-				<h1>Allegra!</h1>
-				<button type="button" class="btn btn-default">Links</button>
-			</div>
-		</div>
-	</div>
+    <!DOCTYPE html>
+    <html>
 
-    <!--jquery-->
-	<script src="http://code.jquery.com/jquery-latest.js"></script>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Willkommen</title>
+    </head>
 
-    <!-- bootstrap.min.css -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <body>
+        <div class="container">
+            <div class="row row-centered">
+                <div class="col-md-12">
+                    <input id="ex8" data-slider-id='ex1Slider' type="text" data-slider-min="0" data-slider-max="20" data-slider-step="1" data-slider-value="14" />
+                </div>
+            </div>
+        </div>
 
-    <!-- bootstrap.min.js -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    
-    <!--eigenes css-->
-    <link rel="stylesheet" href="css/css.css"stylesheet.css"">
-</body>
+        <!--jquery-->
+        <script src="http://code.jquery.com/jquery-latest.js"></script>
 
-</html>
+        <!-- bootstrap.min.css -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+        <!-- bootstrap.min.js -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+        <!--eigenes css-->
+        <link rel="stylesheet" href="css/css.css" stylesheet.css "">
+    </body>
+
+    </html>
