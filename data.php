9ace17bb8d78c1e53bd6b847891d095e49233333
PhilippeZuber <pzuber@hotmail.ch>
2016-11-10 13:40:46 +0100

    Jquery eingebunden

--- data.php
+++ data.php
@@ -4,14 +4,12 @@
     session_destroy();
 
 ?>
+<!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--eigenes css-->
-    <link rel="stylesheet" href="css/css.css"stylesheet.css"">
     <title>Willkommen</title>
 </head>
 <body>
@@ -25,12 +23,17 @@
 	</div>
 
 
-
+    <!--jquery-->
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
+   
     <!-- bootstrap.min.css -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
     <!-- bootstrap.min.js -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    
+    <!--eigenes css-->
+    <link rel="stylesheet" href="css/css.css"stylesheet.css"">
 </body>
 
 </html>
--- people.php
+++ people.php
@@ -4,8 +4,8 @@
     session_destroy();
 
 ?>
+<!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -24,13 +24,17 @@
 		</div>
 	</div>
 
-
+    <!--jquery-->
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
 
     <!-- bootstrap.min.css -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
     <!-- bootstrap.min.js -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    
+    <!--eigenes css-->
+    <link rel="stylesheet" href="css/css.css"stylesheet.css"">
 </body>
 
 </html>
