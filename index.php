91ac9976cb81285c194752bdf81f52bdd18ee580
Simon Sägesser <simi_saegi@hotmail.com>
2016-11-10 09:08:42 +0100

    First_Upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Sprache</title>
+</head>
+
+<body>
+    background-color: #813333;
+
+
+
+
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+</body>
+
+</html>
