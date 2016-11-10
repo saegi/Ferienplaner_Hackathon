f8d7c962a42be33ce576bbd307a80db8e27e68e0
PhilippeZuber <pzuber@hotmail.ch>
2016-11-10 12:49:07 +0100

    Aktueller Stand

--- index.php
+++ index.php
@@ -1,26 +1,30 @@
-<!doctype html>
+<?php 
+	session_start();
+    if(isset($_SESSION['id'])) unset($_SESSION['id']);
+    session_destroy();
+
+?>
 <html>
 
 <head>
-    <meta charset="UTF-8">
-    <title>Sprache</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Willkommen</title>
 </head>
-
 <body>
-    background-color: #813333;
-
-
-    <!--Guguus-->
-
-
+	<div class="container">
+		<h1>Allegra!</h1>
+		<button type="button" class="btn btn-default">Links</button>
+	</div>
 
 
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="js/bootstrap.min.js"></script>
+    <!-- bootstrap.min.css -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <!-- bootstrap.min.js -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </body>
 
 </html>
