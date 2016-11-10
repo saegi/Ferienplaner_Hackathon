a93d637239f3b4d362d92a397187b94150e5b691
Simon Sägesser <simi_saegi@hotmail.com>
2016-11-10 14:45:46 +0100

    budget erneut angepasst

--- budget.php
+++ budget.php
@@ -17,10 +17,10 @@
     <body>
         <div class="container">
             <div class="row row-centered">
-                <div class="col-md-6">
+                <div class="col-md-6 centered">
                     <div class="text_input">
                         <p>Maximales Budget pro Person</p>
-                        <input type="range" name="zahlen" id="regler" min="0" max="2000" value="0" step="10" onchange="showValue(this.value), wechsel()" />
+                        <input type="range" name="zahlen" id="regler" min="0" max="100" value="0" step="5" onchange="showValue(this.value), wechsel()" />
 
                         <span id="kosten">0</span>
                         <script type="text/javascript">
