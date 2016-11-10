7746506da7a48c18e2c2e9eb474c2d3b632d1e45
PhilippeZuber <pzuber@hotmail.ch>
2016-11-10 13:05:39 +0100

    Alle Dateien eingemited

--- people.php
+++ people.php
@@ -1,10 +1,36 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Untitled Document</title>
-</head>
+<?php 
+	session_start();
+    if(isset($_SESSION['id'])) unset($_SESSION['id']);
+    session_destroy();
 
+?>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--eigenes css-->
+    <link rel="stylesheet" href="css/css.css"stylesheet.css"">
+    <title>Willkommen</title>
+</head>
 <body>
+	<div class="container">
+		<div class="row row-centered">
+			<div class="col-md-12">
+				<h1>Allegra!</h1>
+				<button type="button" class="btn btn-default">Links</button>
+			</div>
+		</div>
+	</div>
+
+
+
+    <!-- bootstrap.min.css -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <!-- bootstrap.min.js -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </body>
+
 </html>
