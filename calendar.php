d3fe9029b31f939c7785fe09d122f8ddc51120b0
eazyfish <silvano_pellin@hotmail.com>
2016-11-10 15:03:28 +0100

    Fortschritt Balken

--- calendar.php
+++ calendar.php
@@ -61,6 +61,11 @@
 			</div>
 		</div>
 
+		<div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 25%;">
+  </div>
+</div>
+
 
 
 
