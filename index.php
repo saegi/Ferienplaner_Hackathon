5dc6ff52a72f3eecaba5d4f9dc7194a42d50fd80
PhilippeZuber <pzuber@hotmail.ch>
2016-11-10 13:04:45 +0100

    Alles eingemited

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--eigenes css-->
+    <link rel="stylesheet" href="css/css.css"stylesheet.css"">
     <title>Willkommen</title>
 </head>
 <body>
