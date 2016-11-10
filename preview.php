6baa5a314814eabf6fce54cf2d79a441b5ee4ed5
Simon Sägesser <simi_saegi@hotmail.com>
2016-11-10 15:50:06 +0100

    left_preview

--- preview.php
+++ preview.php
@@ -16,8 +16,8 @@
 
     <body>
         <div class="container">
-            <div class="row row-centered">
-                <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-12" class="left">
                     <h1>Ihre Angaben:</h1>
                     <br>
                     <p>Wochentag:</p>
