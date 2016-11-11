0d7446e82b477ae80ca3a6f538b6bb9d930a08b7
PhilippeZuber <pzuber@hotmail.ch>
2016-11-11 10:59:38 +0100

    db verknüpfung

---
+++ system/data.php
@@ -0,0 +1,30 @@
+<?php
+
+function get_db_connection(){
+    $db = mysqli_connect('localhost', '875960_3_1', 'qEIBX3dsTkF1', '875960_3_1')
+        or die('Fehler beim Verbinden mit dem Datenbankserver.');
+    mysqli_set_charset($db, "utf8");
+    return $db;
+}
+
+function get_result($sql){
+	$db = get_db_connection();
+	/*echo $sql;*/
+	$result = mysqli_query($db, $sql);
+	mysqli_close($db);
+	return $result;	
+}
+
+/***********************/
+/* index.php SQL Statements in Functions Register and Login*/
+/***********************/
+
+function login($email, $password){
+	$sql = "SELECT * FROM User WHERE email = '$email' AND password = '$password';";
+	return get_result ($sql);
+}
+
+function register($username, $email, $password){
+  $sql = "INSERT INTO user (UserName, email, password) VALUES ('$username', '$email', '$password');";
+  return get_result ($sql);
+}
