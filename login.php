e18fa29532e9b626d9cebfdd3092fb4ecdf57723
PhilippeZuber <pzuber@hotmail.ch>
2016-11-11 15:24:35 +0100

    stiyling

--- login.php
+++ login.php
@@ -91,7 +91,7 @@
                                         <div class="form-group">
                                             <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Passwort">
                                         </div>
-                                        <div class="col-xs-6 form-group pull-right">
+                                        <div class="col-xs-6-sm-offset-3 form-group">
                                             <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Einloggen">
                                         </div>
                                     </form>
@@ -111,7 +111,7 @@
                                         </div>
                                         <div class="form-group">
                                             <div class="row">
-                                                <div class="col-sm-6 col-sm-offset-3">
+                                                <div class="col-xs-6-sm-offset-3 form-group">
                                                     <input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Registrieren">
                                                 </div>
                                             </div>
