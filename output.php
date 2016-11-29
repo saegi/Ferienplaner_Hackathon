b7a5f02c961130f6e10c216c386d1a8531cad8c5
eazyfish <silvano_pellin@hotmail.com>
2016-11-29 12:00:58 +0100

    Revert "Output geändert"
    This reverts commit eb4ad0ceeda0e6e1463c571605c97fde28d5b423.

--- output.php
+++ output.php
@@ -47,7 +47,7 @@ if ( isset( $_POST[ 'go-button' ] ) ) {
 					<img src="bilder/web/sourceonestuff.jpg" alt="source_website">
 					<div class="caption">
 						<h3>
-							<?php echo $Aktivitaeten['name'];?>
+							<?php return get_result($Aktivitaeten);?>
 						</h3>
 						<p>
 							<?php echo $Aktivitaeten['beschreibung']; ?>
